53384cd64c5d2a4aa1c0d822fc887f03c5094f8b
Anna Litovskaya <Litovskaya09@gmail.com>
2021-01-14 22:33:14 -0500

    first save

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,35 @@
 class FamilyTree {
-  constructor () {}
+  constructor(name) {
+    this.parentName = name;
+    this.children = [];
+  }
+
+  insert(child) {
+    this.children.push(child);
+  }
+
+  findMember(child) {
+    return new FamilyTree(child);
+    // this.children[this.children.indexOf(child)] = newFamily.children;
+  }
 }
 
 module.exports = FamilyTree;
+
+const szwajkowskis = new FamilyTree('Pops');
+
+szwajkowskis.insert('Mike');
+szwajkowskis.insert('Amy');
+szwajkowskis.insert('Todd');
+
+console.log(szwajkowskis.children);
+
+const mikesFamily = szwajkowskis.findMember('Mike');
+
+console.log(mikesFamily.children);
+
+// mikesFamily.insert('Eliot');
+// mikesFamily.insert('Elise');
+// mikesFamily.insert('Cas');
+// mikesFamily.insert('George');
+// mikesFamily.insert('Lear');
